9605c77a3331b76c248953798db99e4fc2fb5434
Alex <agasparr98@gmail.com>
2020-12-28 12:34:25 +0100

    el comito

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+let maPhrase = "J'aime les tartines et j'en mange ";
+let valeur = "4 par jours";
+console.log(maPhrase+valeur);
+
+let chiffre = 5
+let chiffre2 = 35
+
+console.log(chiffre+chiffre2);
+
+console.log(maPhrase+chiffre);
+
+let salut = "Salut tout le monde"
+console.log(salut);
+
+let orevor = "Aurevoir";
+
+salut = orevor;
+
+
+let quatre = 4
+let six = 6
+
+console.log(quatre > six)
+console.log(quatre < six)
+console.log(quatre == six)
+console.log(quatre != six)
+console.log(quatre === six)
+console.log(quatre !== six)
